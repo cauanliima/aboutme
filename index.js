6904923ef711514fbe02b6a0f0c8519e2ff80f4b
Cauan <cauanmarcelo@hotmail.com>
2022-03-28 23:52:45 -0300

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict'
+const app = new Vue({
+    el: '#app',
+    data() { 
+        return {
+            subpag: "pessoal",
+
+
+    }}, // --- End of data --- //
+
+    computed: {
+
+    }, // --- End of computed --- //
+
+    methods: {
+    }, // --- End of methods --- //
+
+    /** Called after the Vue app has been created. A good place to put startup code */
+    created: function() {
+
+    }, // --- End of created hook --- //
+
+    /** Called once all Vue component instances have been loaded and the virtual DOM built */
+    mounted: function(){
+        const app = this  // Reference to `this` in case we need it for more complex functions
+
+    }, // --- End of mounted hook --- //
+
+}) // --- End of app definition --- //
+
+// EOF
